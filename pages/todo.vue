7c7a7e036a3738beb29cc6af2002140ca4c7ed15
mutta0916 <mutta0916@gmail.com>
2021-09-24 22:26:50 +0900

    todoリスト修正

--- pages/todo.vue
+++ pages/todo.vue
@@ -2,11 +2,15 @@
   <div id="app">
     <div class="content">
       <div class="todo">
-        <input type="text" class="memo" placeholder="ここにやることを入力しよう！">
-        <input type="button" @click="fetchSomething" value="追加！">
-      </div>
-      <div class="list">
-        ここにチェックボックスを表示する
+        <input type="text" class="input" placeholder="ここにやることを入力しよう！" v-model="input">
+        <input type="button" @click="addData" value="追加！">
+        <table>
+          <tr v-for="(row, index) in data" :key="index">
+            <td>{{row}}</td>
+            <input v-if="data.length > 0" type="button" @click="modData" value="変更！">
+            <input v-if="data.length > 0" type="button" @click="delData" value="削除！">
+          </tr>
+        </table>
       </div>
     </div>
   </div>
@@ -17,12 +21,25 @@
 export default {
   data() {
     return {
-      data:''
+      input:'',
+      data: []
     }
   },
   methods: {
     async fetchSomething() {
-      this.data = JSON.stringify(await this.$axios.$get('http://127.0.0.1:8000/api/test'))
+      this.data = JSON.stringify(await this.$axios.$get('http://127.0.0.1:8000/api/test'));
+    },
+    addData: function() {
+      this.data.push(this.input);
+      this.input = '';
+    },
+    modData: function() {
+      this.data.push(this.input);
+      this.input = '';
+    },
+    delData: function() {
+      this.data.splice(this.data.indeOf());
+      this.input = '';
     }
   }
 }
@@ -30,12 +47,24 @@ export default {
 
 <style>
 .content {
-    padding: 20px;
-    text-align: center;
-    background-color: azure;
-    margin: 30px 300px;
+  background-color:#E79460;
+  margin: 0 auto;
+  margin-top: 50px;
+}
+
+.todo {
+  text-align: center;
+  padding: 20px 40px;
+  border: 1px solid black;
+}
+/* .content {
+    padding: 20px; */
+    /* text-align: center; */
+    /* background-color: azure; */
+    /* margin: 30px 300px; */
+    /* margin: 0 auto;
 }
 .memo {
     width: 500px;
-}
+} */
 </style>
